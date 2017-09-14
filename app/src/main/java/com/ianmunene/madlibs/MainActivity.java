d80784aae01b6d7a32e770109ca6660c2f60f27f
Ian Munene <munene.ian@live.com>
2017-09-14 18:19:37 +0300

    recycler view displays an array of strings.

--- app/src/main/java/com/ianmunene/madlibs/MainActivity.java
+++ app/src/main/java/com/ianmunene/madlibs/MainActivity.java
@@ -2,12 +2,33 @@ package com.ianmunene.madlibs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    String[] lists = {"first string", "second string", "third string"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_stories);
+
+        // Setting improves performance if you know the changes in content
+        // do not change layout size of the Recycler View
+        mRecyclerView.setHasFixedSize(true);
+
+        // Use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //speficy an adapter
+        mAdapter = new MyAdapter(lists);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
